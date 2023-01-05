ade29c139eb06b9924a9215af1fd5588bdb659bf
KIM 5EUNG <gangnam0522@gmail.com>
2023-01-05 18:28:03 +0900

    conflict 해겨얼

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const itemsRoutes = require('./itemsRoutes');
+const signUpRouter = require("./signUpRouter");
+const signInRouter = require("./signInRouter");
 
 router.use('/items', itemsRoutes.router);
+router.use("/signup", signUpRouter.router);
+router.use("/signin", signInRouter.router);
 
 module.exports = router;
