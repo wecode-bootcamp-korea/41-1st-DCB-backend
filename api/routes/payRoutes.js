27ac2aa1984fc35370c6aa3914aedf7d19226589
KIM 5EUNG <gangnam0522@gmail.com>
2023-01-08 14:40:42 +0900

    wip

---
+++ api/routes/payRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+// const itemsController = require("../controllers/itemsController");
+
+// router.get("/:itemId", itemsController.getItem);
+
+
+module.exports = {
+  router
+};
