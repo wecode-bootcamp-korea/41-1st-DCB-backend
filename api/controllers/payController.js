1be368275c803fefc94d5cbaf6eba9f65b73a1da
KIM 5EUNG <gangnam0522@gmail.com>
2023-01-11 15:15:01 +0900

    wip

--- api/controllers/payController.js
+++ api/controllers/payController.js
@@ -1,10 +1,16 @@
 const payService = require("../services/payService");
 const { asyncErrorHandler } = require("../middleware/errorHandling");
 
-// const getItem = asyncErrorHandler(async (req, res) => {
-//   const { itemId } = req.params
-//   const item = await itemsService.getItem(itemId);
-//   return res.status(200).json({ data: item })
+// const getCart = asyncErrorHandler(async (req, res) => {
+//   const cartList = await cartService.getCart(req.userId);
+
+//   return res.status(200).json({ data: cartList });
+// });
+
+// const addCart = asyncErrorHandler(async (req, res) => {
+//   const addedCart = await cartService.addCart(req.userId, req.body.itemId, req.body.optionId, req.body.quantity);
+//   console.log(req)
+//   return res.status(201).json({ data: addedCart });
 // });
 
 
--- api/routes/payRoutes.js
+++ api/routes/payRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const payController = require("../controllers/payController");
+const validateToken = require("../middleware/auth");
 
-// router.get("/:itemId", itemsController.getItem);
+// router.get("", validateToken, cartController.getCart);
+// router.post("", validateToken, cartController.addCart);
+// router.patch("", validateToken, cartController.updateQuantity);
+// router.delete("", validateToken, cartController.deleteCart);
 
 
 module.exports = {
