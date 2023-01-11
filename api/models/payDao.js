92a1a541436324017c63e53fa4c7378075a11d50
KIM 5EUNG <gangnam0522@gmail.com>
2023-01-11 18:25:56 +0900

    wip

--- api/models/payDao.js
+++ api/models/payDao.js
@@ -3,13 +3,7 @@ const { myDataSource } = require("./myDataSource");
 const addOrder = async (userId) => {
   const result = await myDataSource.query(
     `
-    INSERT INTO
-      orders o (userId)
-    SELECT
-      u.id
-    FROM
-      users u
-    WHERE u.id = ?
+
     `,
     [userId]
   );
