ebd8fb9a7148b958b534273a09eaac85bbf5974d
KIM 5EUNG <gangnam0522@gmail.com>
2023-01-12 00:39:33 +0900

    Dao 완ㅅ어

--- api/models/payDao.js
+++ api/models/payDao.js
@@ -1,15 +1,48 @@
 const { myDataSource } = require("./myDataSource");
 
-const addOrder = async (userId, itemId, optionId, quantity, points) => {
+const addOrder = async (userId, itemId, optionId, quantity, points, orderNum) => {
+
   const orderId = await myDataSource.query(
     `
     INSERT INTO
-      orders o (user_id, status_id, order_number)
+      orders (user_id, status_id)
     SELECT
-      users.id, order_status.id, 
+      u.id AS userId,
+      os.id AS orderStatusId
+    FROM
+      users u
+    RIGHT JOIN
+      order_status os ON os.id = u.id
+    WHERE
+      u.id = ?
+	  GROUP BY u.id = 1
     `,
-    [userId, itemId, optionId, quantity, points]
+    [userId]
   );
+  const orderIdResult = orderId.insertId;
+
+  const orderItemsId = await myDataSource.query(
+    `
+    INSERT INTO
+      order_items (order_id, item_id, quantity)
+    VALUES
+      (?, ?, ?)
+    `,
+    [orderIdResult, itemId, quantity]
+  );
+  const orderItemsIdResult = orderItemsId.insertId;
+
+  const orderItemOptionsId = await myDataSource.query(
+    `
+    INSERT INTO
+      order_item_options (order_item_id, option_id)
+    VALUES
+      (?, ?)
+    `,
+    [orderItemsIdResult, optionId]
+  );
+
+
   return result;
 };
 
